6b05bf734e5c90fc3e5390212e0f8a98d9f44127
Ben Carroll <benjamin.carroll@gobridgit.com>
2014-10-04 15:44:02 -0400

    Server works now! and has one simple route

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 var express = require('express');
-var fs = require('fs');
-var request = require('request');
-cheerio = require('cheerio');
 var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+var cheerio = require('cheerio');
 var mongoose = require('mongoose');
 var sys = require('sys');
 var oauth = require('oauth');
-var bodyParser = require('body-parser');
 
 var _twitterConsumerKey = "YOURTWITTERCONSUMERKEY";
 var _twitterConsumerSecret = "YOURTWITTERCONSUMERSECRET";
 
 // configuration =================
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;    // set our port
+
+// Routes for our API
+// ================================
+var router = express.Router();   // get an instance of the express router
+
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES ---------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+app.listen(port);
+console.log('Magic happens on port ' + port);
 
 // Connect to the db
 mongoose.connect("mongodb://localhost:27017/Headlines", function(err, db) {
@@ -20,84 +42,3 @@ mongoose.connect("mongodb://localhost:27017/Headlines", function(err, db) {
   }
 });
 
-function logErrors(err, req, res, next) {
-  console.error(err.stack);
-  next(err);
-}
-
-function clientErrorHandler(err, req, res, next) {
-  if (req.xhr) {
-    res.status(500).send({ error: 'Something blew up!' });
-  } else {
-    next(err);
-  }
-}
-
-function errorHandler(err, req, res, next) {
-  res.status(500);
-  res.render('error', { error: err });
-}
-
-app.use(bodyParser);
-app.use(logErrors);
-app.use(clientErrorHandler);
-app.use(errorHandler);
-//app.use(app.router);
-
-
-//routes
-//require('./backend/routes')(app);
-
-function consumer() {
-  return new oauth.OAuth(
-    "https://twitter.com/oauth/request_token", "https://twitter.com/oauth/access_token",
-    _twitterConsumerKey, _twitterConsumerSecret, "1.0A", "http://127.0.0.1:8080", "HMAC-SHA1");
-}
-
-// app.dynamicHelpers({
-//   session: function(req, res){
-//     return req.session;
-//   }
-// });
-
-app.get('/', function(req, res){
-  res.send('Hello World');
-});
-
-app.get('/sessions/connect', function(req, res){
-  consumer().getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
-    if (error) {
-      res.send("Error getting OAuth request token : " + sys.inspect(error), 500);
-    } else {
-      req.session.oauthRequestToken = oauthToken;
-      req.session.oauthRequestTokenSecret = oauthTokenSecret;
-      res.redirect("https://twitter.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);
-    }
-  });
-});
-
-app.get('/sessions/callback', function(req, res){
-  sys.puts(">>"+req.session.oauthRequestToken);
-  sys.puts(">>"+req.session.oauthRequestTokenSecret);
-  sys.puts(">>"+req.query.oauth_verifier);
-  consumer().getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
-    if (error) {
-      res.send("Error getting OAuth access token : " + sys.inspect(error) + "["+oauthAccessToken+"]"+ "["+oauthAccessTokenSecret+"]"+ "["+sys.inspect(results)+"]", 500);
-    } else {
-      req.session.oauthAccessToken = oauthAccessToken;
-      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
-      // Right here is where we would write out some nice user stuff
-      consumer.get("http://twitter.com/account/verify_credentials.json", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
-        if (error) {
-          res.send("Error getting twitter screen name : " + sys.inspect(error), 500);
-        } else {
-          req.session.twitterScreenName = data["screen_name"];
-          res.send('You are signed in: ' + req.session.twitterScreenName)
-        }
-      });
-    }
-  });
-});
-
-console.log('Magic happens on port 8081');
-app.listen(parseInt(8081));
