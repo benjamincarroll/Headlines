b2d55a79f8df7a5487bd56d3d2dce2e9b7e4ccce
Ben Carroll <benjamin.carroll@gobridgit.com>
2014-10-04 15:58:51 -0400

    Moved routing logic to routes.js. For organizational purposes

--- backend/routes.js
+++ backend/routes.js
@@ -1,1 +1,19 @@
 //This is where all of the routes will go
+
+module.exports = function(router) {
+
+  // middleware to use for all requests
+  router.use(function(req, res, next){
+  // this will get called for every api call
+  console.log('Something is happening.');
+
+  //TODO: This is where we can check the request for
+  //      authentication token. So cool!
+  next();
+})
+
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! We rock!' });
+});
+
+}
