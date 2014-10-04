b4829b23de577a138c0d68c5de8738d5d5994f95
Ben Carroll <benjamin.carroll@gobridgit.com>
2014-10-04 16:16:47 -0400

    Attempting to do twitter oauth. Still not working

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var mongoose = require('mongoose');
 var sys = require('sys');
 var oauth = require('oauth');
 
-var _twitterConsumerKey = "YOURTWITTERCONSUMERKEY";
-var _twitterConsumerSecret = "YOURTWITTERCONSUMERSECRET";
+var twitterConsumerKey = "TeMMcBZiE61uQRY6zF2lDDX2m";
+var twitterConsumerSecret = "bB1UWpMM5wEIBakC4bBcRGXkEAjKDfNhwe48LjxNlvspmVaQi6";
 
 // configuration =================
 
@@ -17,13 +17,65 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;    // set our port
 
+// Twitter sign in
+var OAuth = require('oauth').OAuth;
+var oa = new OAuth(
+  "https://api.twitter.com/oauth/request_token",
+  "https://api.twitter.com/oauth/access_token",
+  twitterConsumerKey,
+  twitterConsumerSecret,
+  "1.0",
+  "http://127.0.0.1:8080",
+  "HMAC-SHA1"
+);
+
+app.get('/auth/twitter', function(req, res){
+  oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+    if (error) {
+      console.log(error);
+      res.send("yeah no. didn't work.")
+    }
+    else {
+      req.session.oauth = {};
+      req.session.oauth.token = oauth_token;
+      console.log('oauth.token: ' + req.session.oauth.token);
+      req.session.oauth.token_secret = oauth_token_secret;
+      console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
+      res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
+  }
+  });
+});
+
+app.get('/auth/twitter/callback', function(req, res, next){
+  if (req.session.oauth) {
+    req.session.oauth.verifier = req.query.oauth_verifier;
+    var oauth = req.session.oauth;
+
+    oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier,
+    function(error, oauth_access_token, oauth_access_token_secret, results){
+      if (error){
+        console.log(error);
+        res.send("yeah something broke.");
+      } else {
+        req.session.oauth.access_token = oauth_access_token;
+        req.session.oauth,access_token_secret = oauth_access_token_secret;
+        console.log(results);
+        res.send("worked. nice one.");
+      }
+    }
+    );
+  } else
+    next(new Error("you're not supposed to be here."))
+});
+
 // Routes for our API
 // ================================
 var router = express.Router();   // get an instance of the express router
 
+// most routes are found here
 require('./backend/routes')(router);
 
-// more routes for our API will happen here
+
 
 // REGISTER OUR ROUTES ---------------------
 // all of our routes will be prefixed with /api
