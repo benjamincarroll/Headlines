2c11b3789687098472d506e19b99031eba5e2a5c
Ben Carroll <benjamin.carroll@gobridgit.com>
2014-10-09 20:28:47 -0400

    Created headlines.js

---
+++ backend/models/headlines.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var PageSchema = new Schema({
+  articleId      : Number,
+  userId         : Number,
+  headline       : String,
+  dateCreated    : Number,
+  voteCount      : Number
+});
+
+module.exports = mongoose.model('user', PageSchema);
