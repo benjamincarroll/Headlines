b19c901bf18f858100db6c978439a790ff2d79b9
Ben Carroll <benjamin.carroll@gobridgit.com>
2014-10-18 14:22:54 -0400

    Removed home.js

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.get('/auth/twitter', passport.authenticate('twitter'));
 app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
   res.redirect(req.session.returnTo || '/');
 });
+
 // Routes for our API
 // ================================
 var router = express.Router();   // get an instance of the express router
