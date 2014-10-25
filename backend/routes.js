cef3a37898f852afeb6f23d09bfaab76450e37f4
Ben Carroll <benjamin.carroll@gobridgit.com>
2014-10-25 17:51:48 -0400

    Headlines http get method now works! The number the headlines you want after. So if you want top headlines 20-40 the call would be /headlines/20

--- backend/routes.js
+++ backend/routes.js
@@ -7,23 +7,23 @@ module.exports = function(app) {
 
     // middleware to use for all requests, this will be used for
     // all get/post requests
-    app.use(function(req, res, next) {
-        if (req.method == 'POST'){
-          if (req.isAuthenticated()){
-            // if the request is a POST and they are authenticated,
-            // let them through
-            next();
-          } else {
-            res.json({
-              status: 'failed request',
-              message: 'User is not signed in'
-            })
-          }
-        } else {
-          // If the request is a httpGet. Let anyone through.
-          next();
-        }
-    })
+    // app.use(function(req, res, next) {
+    //     if (req.method == 'POST'){
+    //       if (req.isAuthenticated()){
+    //         // if the request is a POST and they are authenticated,
+    //         // let them through
+    //         next();
+    //       } else {
+    //         res.json({
+    //           status: 'failed request',
+    //           message: 'User is not signed in'
+    //         })
+    //       }
+    //     } else {
+    //       // If the request is a httpGet. Let anyone through.
+    //       next();
+    //     }
+    // })
 
     // Test get request
     app.get('/yay', function(req, res) {
@@ -68,7 +68,6 @@ module.exports = function(app) {
     // post an headline
     app.post('/headline', function(req, res) {
 
-        console.log("Here it is: " + req.body.dateCreated);
         Headline.findOne({
             headline: req.body.headline
         }, function(error, headline) {
@@ -197,68 +196,20 @@ module.exports = function(app) {
             })
     });
 
-  // get the 20 most popular articles
-  app.get('/headlines', function(req, res) {
+  // get the 20 most popular articles, after specified number
+  app.get('/headlines/:number', function(req, res) {
       var limit = 20;
+      var number = req.params.number
 
       Headline.find()
+          .skip(number)
           .limit(limit)
           .sort({
               voteCount: -1
-          })
-          .exec(function(err, headlines) {
+          }).exec(function(err, headlines) {
               if (err) return error_handler(err, req, res);
-
-              // Check to see if there are any more articles to
-              // get
-              var voteCount = headlines[0]["voteCount"];
-              var index = 1;
-              var counter = 1;
-              while(headlines[index]["voteCount"] == voteCount) {
-                counter += 1;
-                index += 1;
-              }
-
-              Headline.find({
-                            "voteCount": voteCount
-                          })
-                          .count()
-                          .exec(function(err, number){
-                            var moreBoolean = false;
-                            if (number > counter){
-                              moreBoolean = true;
-                            } else {
-                              Headline.find({"voteCount": {
-                                // this might cause a bug. Is is greater than?
-                                // or greater than or equal to?
-                                "$gt": voteCount
-                              }})
-                              .limit(limit)
-                              .sort({
-                                voteCount: -1
-                              })
-                              .count()
-                              .exec(function(err, number){
-                                if (err) return error_handler(err, req, res);
-
-                                if (number){
-                                  moreBoolean = true;
-                                }
-                                var jsonObject = {
-                                  headlines : "",
-                                  more : ""
-                                };
-                                jsonObject.headlines = headlines;
-                                if (number){
-                                  jsonObject.more = moreBoolean;
-                                } else {
-                                  jsonObject.more = moreBoolean;
-                                }
-                                res.json(jsonObject);
-                                console.log("Oh fuck yeah fuckers");
-                                  })
-                                }
-                          })
-          })
+              res.json(headlines);
+              console.log("20 headlines have been sent, starting with: " + req.params.number);
+          });
   });
 }
